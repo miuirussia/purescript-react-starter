7afe01b131c2c55522e7bd710a8af758e6c41ec6
Kirill Kuznetsov <kdevlab@yandex.ru>
2023-11-30 09:49:03 +0300

    Simplify resolver

--- eslint/vite-resolver.js
+++ eslint/vite-resolver.js
@@ -1,32 +1,40 @@
 /* eslint-env node */
 const path = require('node:path');
-const fs = require('node:fs');
-const crypto = require('node:crypto');
 const resolve = require('resolve');
 const log = require('debug')('eslint-plugin-import:resolver:vite');
 
 const resolvePath = path.resolve('./resolve.js');
 
+const unsafeResolvePath = (p, file) => {
+  const modulePath = resolve.sync(p, {
+    extensions: ['.mjs', '.js', '.json', '.node'],
+    basedir: path.dirname(path.resolve(file)),
+  });
+
+  log('Resolved to:', modulePath);
+
+  return {
+    found: true,
+    path: modulePath,
+  };
+};
+
 exports.interfaceVersion = 2;
 exports.resolve = function (source, file) {
   log('Resolving:', source, 'from:', file);
 
-  const resolveConfig = require(resolvePath)(p => path.resolve(path.dirname(resolvePath), p));
-  log(resolveConfig);
+  const resolveConfig = require(resolvePath)((p) => path.resolve(path.dirname(resolvePath), p));
   try {
     const aliasPath = resolveConfig[source];
     if (aliasPath) {
-      const modulePath = resolve.sync(aliasPath, {
-        extensions: ['.mjs', '.js', '.json', '.node'],
-        basedir: path.dirname(path.resolve(file)),
-      });
-
-      log('Resolved to:', modulePath);
-
-      return {
-        found: true,
-        path: modulePath,
-      };
+      return unsafeResolvePath(aliasPath, file);
+    } else if (source.startsWith('@/')) {
+      const aliasPath = resolveConfig['@/'];
+      if (aliasPath) {
+        return unsafeResolvePath(`${aliasPath}${source.substring(2)}`, file);
+      } else {
+        return { found: false, path: null };
+      }
     } else {
       return { found: false, path: null };
     }
